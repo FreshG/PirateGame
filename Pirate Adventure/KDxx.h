bf7134fe71fc222680b68b27725fd5b5056d0690
Kai Dickmann <dickmann@hmmdeutschland.de>
2014-09-11 10:15:40 +0200

    added test

---
+++ Pirate Adventure/KDxx.h
@@ -0,0 +1,13 @@
+//
+//  KDxx.h
+//  Pirate Adventure
+//
+//  Created by Kai Dickmann on 11.09.14.
+//  Copyright (c) 2014 Kai Dickmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KDxx : NSObject
+
+@end
